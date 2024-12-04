fe28c380bad63a94686726fa4c640b95151ee4c4
cz0r3k <czorek@protonmail.com>
2024-12-04 16:43:47 +0100

    add google auth to web_admin

--- web_admin/Program.cs
+++ web_admin/Program.cs
@@ -1,13 +1,14 @@
-using web_admin.Components;
+﻿using web_admin.Components;
 using GrpcGreeter;
 using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authentication.OAuth;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
 
 builder.Services
     .AddGrpcClient<Greeter.GreeterClient>(options =>
@@ -17,13 +18,51 @@ builder.Services
     .ConfigurePrimaryHttpMessageHandler(
         () => new GrpcWebHandler(new HttpClientHandler()));
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
+})
+.AddCookie()
+.AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    googleOptions.Scope.Add("email");
+    googleOptions.Scope.Add("profile");
+    googleOptions.SaveTokens = true;
+    googleOptions.CallbackPath = "/signin-google";
+    googleOptions.Events = new OAuthEvents
+    {
+        OnRedirectToAuthorizationEndpoint = context =>
+        {
+            context.Response.Redirect(context.RedirectUri + "&prompt=consent");
+            return Task.CompletedTask;
+        }
+    };
+});
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    var securePolicy = new AuthorizationPolicyBuilder()
+//        .RequireAuthenticatedUser()
+//        .Build();
+
+//    options.AddPolicy("SecurePolicy", securePolicy);
+//});
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddHttpContextAccessor();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -31,6 +70,9 @@ app.UseHttpsRedirection();
 
 
 app.UseAntiforgery();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
