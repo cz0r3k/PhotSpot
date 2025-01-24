2883815c2f4a0288cd95800a5b5e9101b0db06f8
cz0r3k <czorek@protonmail.com>
2025-01-24 18:01:37 +0100

    add like button

--- mobile_app/BrowsePhotosPage.xaml.cs
+++ mobile_app/BrowsePhotosPage.xaml.cs
@@ -1,4 +1,7 @@
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Grpc.Core;
 using Grpc.Net.Client;
 using GrpcPhoto;
@@ -11,8 +14,6 @@ namespace QRtest
         public ObservableCollection<ImageInfo> Images { get; private set; } = new();
         public string EventName { get; set; }
 
-        //private bool IsRefreshing { get; set; }
-
         public BrowsePhotosPage()
         {
             InitializeComponent();
@@ -20,8 +21,72 @@ namespace QRtest
             EventName = EventInfoManager.EventName;
             BindingContext = this;
             refreshView.IsRefreshing = true;
-            //IsRefreshing = true;
-            //LoadImages();
+        }
+
+        private async void ToggleLike(object sender, EventArgs e)
+        {
+            var imageInfo = ImageCarousel.CurrentItem as ImageInfo;
+            if (imageInfo.IsLiked)
+            {
+                await Like();
+            }
+            else
+            {
+                await Unlike();
+            }
+        }
+
+        private async Task Like()
+        {
+            var imageInfo = ImageCarousel.CurrentItem as ImageInfo;
+            Images[ImageCarousel.Position].IsLiked = true;
+            Images[ImageCarousel.Position].Likes += 1;
+            var socketHttpHandler = new SocketsHttpHandler
+            {
+                SslOptions = new System.Net.Security.SslClientAuthenticationOptions
+                {
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+                },
+                EnableMultipleHttp2Connections = true
+            };
+
+            using var channel = GrpcChannel.ForAddress($"https://{Globals.IP_ADDRESS}:7244", new GrpcChannelOptions
+            {
+                HttpHandler = socketHttpHandler
+            });
+            var client = new Photo.PhotoClient(channel);
+            var result = await client.LikePhotoInsecureAsync(new LikePhotoInsecureRequest
+            {
+                Email = "czorek@protonmail.com", PhotoId = new UUID { Value = imageInfo.Id.ToString() },
+                EventId = new GrpcEvent.UUID { Value = imageInfo.Id.ToString() }
+            });
+        }
+
+        private async Task Unlike()
+        { 
+            var imageInfo = ImageCarousel.CurrentItem as ImageInfo;
+            Images[ImageCarousel.Position].IsLiked = false;
+            Images[ImageCarousel.Position].Likes -= 1;
+            
+            var socketHttpHandler = new SocketsHttpHandler
+            {
+                SslOptions = new System.Net.Security.SslClientAuthenticationOptions
+                {
+                    RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+                },
+                EnableMultipleHttp2Connections = true
+            };
+
+            using var channel = GrpcChannel.ForAddress($"https://{Globals.IP_ADDRESS}:7244", new GrpcChannelOptions
+            {
+                HttpHandler = socketHttpHandler
+            });
+            var client = new Photo.PhotoClient(channel);
+            var result = await client.UnLikePhotoInsecureAsync(new LikePhotoInsecureRequest
+            {
+                Email = "czorek@protonmail.com", PhotoId = new UUID { Value = imageInfo.Id.ToString() },
+                EventId = new GrpcEvent.UUID { Value = imageInfo.Id.ToString() }
+            });
         }
 
         private async void refreshView_Refreshing(object sender, EventArgs e)
@@ -31,10 +96,10 @@ namespace QRtest
                 var link =
                     $"http://{Globals.IP_ADDRESS}:10000/devstoreaccount1/photos/{EventInfoManager.EventId}/{photoGuid}.png";
                 var info = await GetLikesInfoAsync(photoGuid);
-                Images.Add(new ImageInfo { Id = photoGuid, Link = link, IsLiked = info.IsUserLike, Likes = info.Likes });
-                DisplayAlert("Refresh", $"Photo {photoGuid} has been refreshed Likes = {info.Likes}", "OK");
+                Images.Add(new ImageInfo
+                    { Id = photoGuid, Link = link, IsLiked = info.IsUserLike, Likes = info.Likes });
             }
-            
+
 
             refreshView.IsRefreshing = false;
         }
